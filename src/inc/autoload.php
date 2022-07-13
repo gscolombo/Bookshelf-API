a8470f5c1272c29ea636a3fb7cd5b7a6a2db69fe
Gabriel <gabriel.souza.colombo13@gmail.com>
2022-07-13 12:42:24 -0300

    Adjustments

--- src/inc/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    spl_autoload_register(function($class_name) {
-        $path = str_replace("\\", DIRECTORY_SEPARATOR, ROOT_PATH . $class_name . ".php");
-        if (file_exists($path)) {
-            require_once $path;
-        }
-    });
-?>
--- src/inc/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    const DB_USER = "root";
-    const DB_PASSWORD = "root";
-    const DB_DSN = "mysql:dbname=library;host=db";
-?>
